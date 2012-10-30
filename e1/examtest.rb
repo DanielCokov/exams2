147b20fea31dbf2a170dd018ed942ea8d72fb37c
dani <dani@dani-X51R.(none)>
2012-10-30 11:27:52 +0200

    exams

---
+++ e1/examtest.rb
@@ -0,0 +1,33 @@
+require 'csv'
+
+
+
+f_name = "feature_scenarios_times.csv"
+
+f_name2 = "izpit_result.txt"
+
+
+
+count = 0
+
+
+
+ File.open(f_name2, 'w') do |f2|
+
+    CSV.foreach(f_name) do |row|
+
+        if row[1] == "features/facebook_share.feature"
+
+            count = count + 1; 
+
+        end
+
+    end
+
+ f2.puts count.to_s
+
+ end 
+
+
+
+
